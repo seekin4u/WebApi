46265f24caec12306bde60aff94a08d5ed1880d7
seekin4u <wagamama47@gmail.com>
2019-05-04 14:57:12 +0300

    Header and footer for project

---
+++ WebApi/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class IndexController : Controller
+    {
+        [Route("[controller]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
