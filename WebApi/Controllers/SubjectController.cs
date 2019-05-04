e90ccdf463a1b13c255e650ad46e0764c8d6afb6
seekin4u <wagamama47@gmail.com>
2019-05-04 14:56:50 +0300

    2 controllers

---
+++ WebApi/Controllers/SubjectController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Contexts;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class SubjectController : Controller
+    {
+        private ApplicationContext db;
+
+        public SubjectController(ApplicationContext ac)
+        {
+            db = ac;
+        }
+
+        [Route("[controller]")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Subjects.ToListAsync());
+        }
+
+
+        [Route("[controller]/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Route("[controller]/create")]
+        [HttpPost]
+        public async Task<IActionResult> Create(Hall hall)
+        {
+            db.Hall.Add(hall);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
